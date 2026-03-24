35f9541878a4efcca7a80d3f4393c93bbe7941b5
Keerthivarman316 <keerthivarmansasikumar@gmail.com>
2026-03-24 13:55:12 +0530

    Trained Random Forest Model with Engineerd features

---
+++ src/train_rf.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+import joblib
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.join(script_dir, "../data/Processed/Kaveri_Delta_Soil_Moisture.csv")
+models_dir = os.path.join(script_dir, "../models")
+model_path = os.path.join(models_dir, "random_forest_model.pkl")
+
+os.makedirs(models_dir, exist_ok=True)
+
+df = pd.read_csv(data_path)
+x = df[['VV', 'VH', 'NDVI', 'VV_VH_ratio', 'NDVI_VV']]
+y = df['soil_moisture']
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+model = RandomForestRegressor(n_estimators=200, max_depth=10, random_state=42)
+model.fit(X_train, y_train)
+
+joblib.dump(model, model_path)
+print("Model trained and saved successfully")
